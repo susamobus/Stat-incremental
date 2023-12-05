e6848ddae3b7703dfc5b5fdfffe1f0f7914f16ae
Test <101502649+susamobus@users.noreply.github.com>
2023-12-05 17:18:18 +0800

    Update points.js

--- points.js
+++ points.js
@@ -2,7 +2,7 @@ let points = 0
 
 function GainPoints() {
   if (points == 0) {
-    document.getElementById("maindisplay").style.display = "block"
+    document.getElementById("maindisplay").style.display = "inline-block"
   }
   points += 1
 }
