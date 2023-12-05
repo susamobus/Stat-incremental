23cd02931ba9458f7c8663b6130482b5b4b1a176
Test <101502649+susamobus@users.noreply.github.com>
2023-12-05 17:04:22 +0800

    Update Points.js

--- Points.js
+++ Points.js
@@ -2,7 +2,7 @@ let points = 0
 
 function GainPoints() {
   if (points == 0) {
-    document.getElementById("MainDisplay").style.display = "flex"
+    document.getElementById("maindisplay").style.display = "flex"
   }
   points += 1
 }
