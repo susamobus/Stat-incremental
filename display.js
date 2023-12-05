38ebd9e7041e1de5d5509807b7415326a48d9b23
Test <101502649+susamobus@users.noreply.github.com>
2023-12-05 18:12:24 +0800

    Update display.js

--- display.js
+++ display.js
@@ -9,40 +9,44 @@ function DisplayTick() {
 }
 
 function ToggleTheme() {
-  let color1; //Contrast
-  let color2; //Similar
-  if (Theme == "light") {
-    Theme = "dark";
-    color1 = "white";
-    color2 = "black";
-  } else {
-    Theme = "light";
-    color1 = "black";
-    color2 = "white";
-  }
-  let containers = document.getElementsByClassName("Containers")
-  let buttons = document.getElementsByClassName("Buttons")
-  let text = document.getElementsByClassName("Text")
-  let containertext = document.getElementsByClassName("ContainerText")
-  const lengtharray = [containers.length,buttons.length,text.length,containertext.length]
-  for (let i = 0; i < lengtharray[0]; i++) {
-    let container = containers[i]
-    container.style.color = color2;
-    container.style.backgroundColor = color1;
-    container.style.borderColor = color2
-  }
-  for (let i = 0; i < lengtharray[1]; i++) {
-    let button = buttons[i]
-    button.style.color = color1;
-    button.style.backgroundColor = color2;
-    button.style.borderColor = color1;
-  }
-  for (let i = 0; i < lengtharray[2]; i++) {
-    text[i].style.color = color1;
-  }
-  for (let i = 0; i < lengtharray[3]; i++) {
-    containertext[i].style.color = color2;
-  }
+  let timer;
+  clearTimeout(timer);
+  timer = setTimeout(() => {
+    let color1; //Contrast
+    let color2; //Similar
+    if (Theme == "light") {
+      Theme = "dark";
+      color1 = "white";
+      color2 = "black";
+    } else {
+      Theme = "light";
+      color1 = "black";
+      color2 = "white";
+    }
+    let containers = document.getElementsByClassName("Containers")
+    let buttons = document.getElementsByClassName("Buttons")
+    let text = document.getElementsByClassName("Text")
+    let containertext = document.getElementsByClassName("ContainerText")
+    const lengtharray = [containers.length,buttons.length,text.length,containertext.length]
+    for (let i = 0; i < lengtharray[0]; i++) {
+      let container = containers[i]
+      container.style.color = color2;
+      container.style.backgroundColor = color1;
+      container.style.borderColor = color2
+    }
+    for (let i = 0; i < lengtharray[1]; i++) {
+      let button = buttons[i]
+      button.style.color = color1;
+      button.style.backgroundColor = color2;
+      button.style.borderColor = color1;
+    }
+    for (let i = 0; i < lengtharray[2]; i++) {
+      text[i].style.color = color1;
+    }
+    for (let i = 0; i < lengtharray[3]; i++) {
+      containertext[i].style.color = color2;
+    }
+  }, 200);
 }
 
 function ToggleDisplayTick() {
