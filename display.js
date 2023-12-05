75fecfbdf0c1b2371261cddbab7aaa5e9dff393a
Test <101502649+susamobus@users.noreply.github.com>
2023-12-05 17:57:46 +0800

    Update display.js

--- display.js
+++ display.js
@@ -1,9 +1,47 @@
 let DisplayTicking;
 let DisplayInterval = 50;
+let Appearance = "light";
 
 function DisplayTick() {
   if (points != 0) {
     document.getElementById("pointdisplay").innerHTML = "Points: " + points
+  }
+}
+
+function ToggleAppearance() {
+  let color1;
+  let color2;
+  if (Appearance == "light") {
+    Appearance = "dark";
+    color1 = "white";
+    color2 = "black";
+  } else {
+    Appearance = "light";
+    color1 = "black";
+    color2 = "white";
+  }
+  let containers = document.getElementsByClassName("Containers")
+  let buttons = document.getElementsByClassName("Buttons")
+  let text = document.getElementsByClassName("Text")
+  let containertext = document.getElementsByClassName("ContainerText")
+  const lengtharray = [containers.length,buttons.length,text.length,containertext.length]
+  for (let i = 0; i < lengtharray[0]; i++) {
+    let container = containers[i]
+    container.style.color = color1;
+    container.style.backgroundColor = color2;
+    container.style.borderColor = color1;
+  }
+  for (let i = 0; i < lengtharray[1]; i++) {
+    let button = buttons[i]
+    button.style.color = color2;
+    button.style.backgroundColor = color1;
+    button.style.borderColor = color2;
+  }
+  for (let i = 0; i < lengtharray[2]; i++) {
+    text[i].style.color = color1;
+  }
+  for (let i = 0; i < lengtharray[3]; i++) {
+    containertext[i].style.color = color2;
   }
 }
 
