9cf7c9c0060a763d0775ef221648feca281a5b7d
Test <101502649+susamobus@users.noreply.github.com>
2023-12-06 10:41:25 +0800

    Update points.js

--- points.js
+++ points.js
@@ -1,17 +1,8 @@
 let points = 0
 
-function RevealMainDisplay() {
-  let maindisp = document.getElementById("maindisplay")
-  maindisp.style.display = "block"
-  maindisp.style.transitionDuration = FadeInDuration + "s"
-  setTimeout(function() {
-    maindisp.style.opacity = "1";
-  }, FadeInDuration * 1000);
-}
-
 function GainPoints() {
   if (points == 0) {
-    RevealMainDisplay()
+    FadeInTabs("MainDisplay")
   }
   points += 1
 }
